c6771182a271f4e434bc847a5cf7cc77cd759dac
aled96 <aled96@hotmail.it>
2018-03-20 11:25:00 +0100

    Upload Initial File

---
+++ js/common.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {   
+            var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+            var sel2 = sideslider.attr('data-target-2');
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+                $(sel2).toggleClass('out');
+            });
+        });
+
+$('.nav-collapse').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    return false;
+});
+
+function show(target) {
+	document.getElementById(target).style.visibility = 'visible';
+};
+
+function hide(target) {
+	document.getElementById(target).style.visibility = 'hidden';
+};
+
+function autoHeight() {
+	document.getElementById("autoHeight").style.height = 'auto';
+};
