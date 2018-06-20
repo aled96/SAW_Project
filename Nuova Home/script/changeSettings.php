2bdc3431056430738d833a4ec9efe6da77da9538
Marco Lapolla <marco.lapolla5@gmail.com>
2018-06-20 13:43:48 +0200

    Fix errore in setting

--- Nuova Home/script/changeSettings.php
+++ Nuova Home/script/changeSettings.php
@@ -13,7 +13,13 @@ $surname = $conn->real_escape_string($_POST['surnameChange']);
 $gender = $conn->real_escape_string($_POST['gender']);
 $date_of_birth = $conn->real_escape_string($_POST['dateChange']);
 $city = $conn->real_escape_string($_POST['cityChange']);
-$img = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+
+if ($_FILES['image']['size'] == 0 && $_FILES['image']['error'] == 0){
+    $img = null;
+}
+else{
+	$img = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+}
 
 if($img == null)
 	$sql = "UPDATE user SET Email = '".$email."', Name = '".$name."', Surname = '".$surname."',Gender = '".$gender."',Date_of_birth = '".$date_of_birth."', City = '".$city."' WHERE Username = '".$user."'";
