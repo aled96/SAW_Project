aa9665c5db9e7b15bb7222025bc8ee006195b37d
aled96 <aled96@hotmail.it>
2018-06-24 18:58:22 +0200

    Profile Pic Chat aggiornata (presa da DB)

--- Nuova Home/chat.php
+++ Nuova Home/chat.php
@@ -39,7 +39,7 @@ if($result->num_rows == 0) {
     <div class="container snippet">
     </div>';
 }
-else {
+else {	
     echo '
     <div class="container snippet">
         <div class="row">
@@ -58,36 +58,47 @@ else {
                                 </thead>
                                 <tbody id="tbody_chat">';
 
+								while ($row = mySQLi_fetch_array($result)) {
+									if (strcmp($_SESSION['username'], $row['User_from']) == 0)
+										$other = $row['User_to'];
+									else
+										$other = $row['User_from'];
+																			
+									$key = array_search($other, $list_users);
+									if ($key !== false) {
+										continue;
+									}
+									array_push($list_users, $other);
 
-                                    while ($row = mySQLi_fetch_array($result)) {
-                                        if (strcmp($_SESSION['username'], $row['User_from']) == 0)
-                                            $other = $row['User_to'];
-                                        else
-                                            $other = $row['User_from'];
-
-                                        $key = array_search($other, $list_users);
-                                        if ($key !== false) {
-                                            continue;
-                                        }
-                                        array_push($list_users, $other);
-
-                                        $sql2 = "SELECT COUNT(*) as count FROM chat WHERE Is_read = false and User_from = '" . $other . "' and User_to = '" . $user . "'";
-                                        $result2 = mySQLi_query($conn, $sql2) or die("Error query");
-                                        $row2 = mySQLi_fetch_array($result2);
-                                        $unread_count = $row2['count'];
-                                        echo '
-                                            <tr><td>
-                                                <img class="mini-image" src="https://bootdey.com/img/Content/user_1.jpg" alt="">
-                                                <a class="name" href="view_chat.php?user_to=' . $other . '">' . $other . '</a>
-                                            </td>
-                                            <td>' . $row['max_date'] . '</td>
-                                            <td id="status' . $other . '" class="text-center">';
-                                        if ($unread_count == 0)
-                                            echo '<span class="label label-success">No Unread</span>';
-                                        else
-                                            echo '<span class="label label-warning">Unread (' . $unread_count . ')</span>';
-                                        echo '</td>
-                                            </tr>';
+									
+									$otherPic = "https://bootdey.com/img/Content/user_1.jpg" ;
+									
+									$sql2 = "SELECT distinct ProfilePic FROM user WHERE Username = '".$other."'";
+									$result2 = mySQLi_query($conn, $sql2) or die("Error query1");
+									
+									$row2 = mySQLi_fetch_array($result2);
+									if($result2->num_rows > 0)
+										$otherPic = "data:image/jpeg;base64,".base64_encode($row2['ProfilePic']);
+									
+									
+									$sql3 = "SELECT COUNT(*) as count FROM chat WHERE Is_read = false and User_from = '" . $other . "' and User_to = '" . $user . "'";
+									$result3 = mySQLi_query($conn, $sql3) or die("Error query");
+									$row3 = mySQLi_fetch_array($result3);
+									$unread_count = $row3['count'];
+									
+									echo '
+										<tr><td>
+											<img src="'.$otherPic.'" alt="" class="mini-image"/>
+											<a class="name" href="view_chat.php?user_to=' . $other . '">' . $other . '</a>
+										</td>
+										<td>'.$row['max_date'].'</td>
+										<td id="status' . $other . '" class="text-center">';
+									if ($unread_count == 0)
+										echo '<span class="label label-success">No Unread</span>';
+									else
+										echo '<span class="label label-warning">Unread (' . $unread_count . ')</span>';
+									echo '</td>
+										</tr>';
                                 }
 
                                 echo'
@@ -98,11 +109,10 @@ else {
                 </div>
             </div>
         </div>
-    </div>
-                                    ';
-                            }
+    </div>';
+}
 
-                                ?>
+	?>
 
 
 
