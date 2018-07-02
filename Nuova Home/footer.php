5407f056c86dc6cfdfe3f91a9c45a1c71e5b0625
Alessio De Luca <aled96@hotmail.it>
2018-07-02 15:55:26 +0200

    Footer icons (color)

--- Nuova Home/footer.php
+++ Nuova Home/footer.php
@@ -1,4 +1,25 @@
 <!-- FOOTER -->
+<script>
+function fb(){
+	document.getElementById("fb").style.color = "cornflowerblue";
+}
+
+function twitter(){
+	document.getElementById("tw").style.color = "darkturquoise";
+}
+
+function link(){
+	document.getElementById("li").style.color = "deepskyblue";
+}
+function email(){
+	document.getElementById("em").style.color = "goldenrod";
+}
+
+function exit(id){
+	document.getElementById(id).style.color = "white";
+}
+</script>
+
     <section id="footer">
         <div class="footer-bottom">
             <div class="container">
@@ -6,10 +27,10 @@
                     <div class="col-md-12">
                         <div class="col-xs-6 copyright">© 2018 Lapolla Marco, De Luca Alessio - Rights Reserved </div>
                         <div class="col-xs-6 footer-icon">
-                            <i class="fa fa-2x fa-facebook-square"></i>
-                            <i class="fa fa-2x fa-twitter-square"></i>
-                            <i class="fa fa-2x fa-linkedin-square"></i>
-                            <i class="fa fa-2x fa-envelope-o"></i>
+                            <i class="fa fa-2x fa-facebook-square" id="fb" onmouseover="fb();" onmouseout="exit('fb');"></i>
+                            <i class="fa fa-2x fa-twitter-square" id="tw" onmouseover="twitter();" onmouseout="exit('tw');"></i>
+                            <i class="fa fa-2x fa-linkedin-square" id="li" onmouseover="link();" onmouseout="exit('li');"></i>
+                            <i class="fa fa-2x fa-envelope-o" id="em" onmouseover="email();" onmouseout="exit('em');"></i>
                         </div>
                     </div>
                 </div>
